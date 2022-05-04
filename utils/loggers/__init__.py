3a5ae2419c4d925392c7cb950878c82c64a297fd
Federico Garro <federico.garro@epfl.ch>
2022-05-04 10:51:26 +0200

    Added wandb (Weight and Biases) logging

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -25,7 +25,7 @@ try:
     assert hasattr(wandb, '__version__')  # verify package import not local dir
     if pkg.parse_version(wandb.__version__) >= pkg.parse_version('0.12.2') and RANK in [0, -1]:
         try:
-            wandb_login_success = wandb.login(timeout=30)
+            wandb_login_success = wandb.login(key="4893a49e39ba791d5d4c6c1eec868f2c99da0172", timeout=30)
         except wandb.errors.UsageError:  # known non-TTY terminal issue
             wandb_login_success = False
         if not wandb_login_success:
